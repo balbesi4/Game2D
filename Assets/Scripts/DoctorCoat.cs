924e52da969237575db6e15c276bc96b9801c4e0
Arseniy_Yolkin <arsreniy.yolkin@yandex.ru>
2023-06-02 04:10:55 +0500

    still finishing

--- Assets/Scripts/DoctorCoat.cs
+++ Assets/Scripts/DoctorCoat.cs
@@ -11,8 +11,13 @@ public class DoctorCoat : MonoBehaviour
     public Text CutSceneText;
     public GameObject LeftButton, RightButton, ExitPages;
     public Text LeftText, RightText;
+    public SpriteRenderer Coat;
+    public Sprite CoatSprite;
+    public GameObject Trigger;
+    public GameObject Player;
 
     private int pageIndex = 0;
+    private int phrazeIndex = 0;
     private bool isCutSceneGoing = false;
 
     private string[] phrazes = new[]
@@ -24,11 +29,32 @@ public class DoctorCoat : MonoBehaviour
         "Надеюсь, что в прошлом какая-то часть записей останется"
     };
 
+    private string[] pages = new[]
+    {
+        "21.05.68\nВеду разработку вакцины, усиляющей человека в 4 раза",
+        "01.07.68\nЯ испытал вакцину на кролике, но забыл, что она для людей",
+
+        "14.07.68\nВклоллол сее вкацииын",
+        ""
+    };
+
     private void Update()
     {
         if (!isCutSceneGoing)
         {
             ManagePages();
+        }
+        else
+        {
+            CutSceneText.text = phrazes[phrazeIndex];
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if (phrazeIndex == phrazes.Length - 1)
+                    FinishCutScene();
+                else
+                    phrazeIndex++;
+            }
         }
     }
 
@@ -55,6 +81,21 @@ public class DoctorCoat : MonoBehaviour
         Destroy(RightText.gameObject);
         Destroy(ExitPages);
 
+        foreach (var obj in CutSceneObjects)
+            obj.SetActive(true);
+        CutSceneText.gameObject.SetActive(true);
+    }
 
+    private void FinishCutScene()
+    {
+        Coat.sprite = CoatSprite;
+        Destroy(Trigger);
+
+        Player.GetComponent<PlayerAnimation>().IsFreezed = false;
+        Player.GetComponent<PlayerMovement>().IsFreezed = false;
+        Player.GetComponent<ShootingControl>().IsFreezed = false;
+        FindObjectOfType<InventoryManagement>().IsFreezed = false;
+
+        Destroy(gameObject);
     }
 }
